8d82657ce8e4b6b74b9f02c96b370d1dda422451
kamalmollav <kamalfairoz127@gmail.com>
2023-10-04 18:34:46 +0530

    my eigth project

---
+++ src/main/java/com/numetry/Jquery/JqueryImplApplication.java
@@ -0,0 +1,13 @@
+package com.numetry.Jquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqueryImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JqueryImplApplication.class, args);
+	}
+
+}
